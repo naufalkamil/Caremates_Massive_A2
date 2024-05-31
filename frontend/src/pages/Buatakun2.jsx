3733f852e851a75e2e937b1310563dff855a813e
lulu1403 <kulumfatma14@gmail.com>
2024-05-31 08:43:39 +0700

    add new page pengajuanpenggalangan

--- frontend/src/pages/Buatakun2.jsx
+++ frontend/src/pages/Buatakun2.jsx
@@ -112,13 +112,13 @@ const Buatakun2 = ({ onNavigate }) => {
 
 // Objek gaya untuk latar belakang
 const backgroundStyle = {
-  height: '100vh', // Memastikan latar belakang menutupi seluruh tinggi viewport
+  height: '100vh', 
   background: 'linear-gradient(45deg, #00cbb7, #0098d9, #00baa7, #0081b8)',
   animation: 'colorChange 12s ease-in-out infinite',
   backgroundSize: '200% 200%',
   backgroundPosition: '0',
   fontFamily: '"Open Sans", sans-serif',
-  margin: 0, // Menghapus margin default dari body
+  margin: 0,
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
@@ -163,7 +163,7 @@ const pnoStyle = {
 
 
 const buttonContainerStyle = {
-  marginTop: 'auto', // Pushes the button container to the bottom
+  marginTop: 'auto', 
   width: '100%',
   display: 'flex',
   justifyContent: 'space-around',
@@ -177,7 +177,7 @@ const button1Style = {
   backgroundColor: '#fff',
   color: '#4D96B6',
   cursor: 'pointer',
-  fontSize: '20px'
+  fontSize: '18px'
 };
 
 export default Buatakun2;
